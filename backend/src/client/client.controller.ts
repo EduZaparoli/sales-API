f28da3bd3a2a2bc0348c721a9b2706667c5544f9
Eduardo Zaparoli <eduzzx1204@gmail.com>
2024-05-31 16:32:50 -0300

    update tables and create metods

--- backend/src/client/client.controller.ts
+++ backend/src/client/client.controller.ts
@@ -5,13 +5,23 @@ import { ClientService } from "./client.service";
 export class ClientController {
 	constructor(private readonly clientService: ClientService) {}
 
-	@Get("cpf/:cpf")
-	findByDocumentNumber(@Param("cpf") cpf: string) {
-		return this.clientService.findByDocumentNumber(cpf);
+	@Get("documentNumber/:documentNumber")
+	findByDocumentNumber(@Param("documentNumber") documentNumber: string) {
+		return this.clientService.findByDocumentNumber(documentNumber);
 	}
 
 	@Get(":id")
 	findOne(@Param("id") id: string) {
 		return this.clientService.findOne(+id);
+	}
+
+	@Get("purchase/:id")
+	async getPurchase(@Param("id") id: string) {
+		return this.clientService.findPurchaseById(+id);
+	}
+
+	@Get("purchases/all")
+	async getAllPurchases() {
+		return this.clientService.findAllPurchases();
 	}
 }
